6450f48fe6f843e47df900118f990326b966fdd7
nilton <niltoenrique3000@hotmail.com>
2024-11-01 15:36:46 +0100

    pages

--- src/App.js
+++ src/App.js
@@ -6,12 +6,12 @@ import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import { EscuelaDePadresPage } from './pages/header/enlacesRodondosPages/escuelaDePadresPage';
 import { ZonaPrivadaPage } from './pages/header/enlacesRodondosPages/zonaPrivadaPage';
 import { ContactoPage } from './pages/header/enlacesRodondosPages/contactoPage';
-import { InicioPage } from './pages/main/inicioPage';
-import { QuienesSomosPage } from './pages/main/quienesSomosPage';
-import { ProyectoPage } from './pages/main/proyectoPage';
-import { InstalacionesPage } from './pages/main/instalacionesPage';
-import { InglesPage } from './pages/main/inglesPage';
-import { EquipoPage } from './pages/main/equipoPage';
+import { InicioPage } from './pages/header/bannerPrincipalCelestePages/inicioPage';
+import { QuienesSomosPage } from './pages/header/bannerPrincipalCelestePages/quienesSomosPage';
+import { ProyectoPage } from './pages/header/bannerPrincipalCelestePages/proyectoPage';
+import { InstalacionesPage } from './pages/header/bannerPrincipalCelestePages/instalacionesPage';
+import { InglesPage } from './pages/header/bannerPrincipalCelestePages/inglesPage';
+import { EquipoPage } from './pages/header/bannerPrincipalCelestePages/equipoPage';
 
 function App() {
   return (
